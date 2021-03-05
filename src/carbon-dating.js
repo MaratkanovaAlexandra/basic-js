6ce2c4c7b29fd004980edc0a42a54add12d43bc2
Aleksandra Maratkanova <ola.maratkanova@gmail.com>
2021-03-05 13:57:52 +0400

    add 1.1

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,14 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity) {
+  if(typeof sampleActivity !== 'string'){return false}
+  else{
+  let numder = +sampleActivity;
+  if((+sampleActivity == NaN)|(+sampleActivity<1)|(+sampleActivity > MODERN_ACTIVITY)){return false};
+
+  let age = Math.log(MODERN_ACTIVITY/numder)/(0.693/HALF_LIFE_PERIOD);
+  if(~~(age) + 1 == 1){return false}
+  return ~~(age) + 1;}
   // remove line with error and write your code here
 };
